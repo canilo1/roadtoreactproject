da1bf51aa72198ec0e69353d96512dc0d607bd52
canilo <carlos.will51@gmail.com>
2023-10-10 20:02:53 -0700

    Update App.js

--- road-toreact-project/src/App.js
+++ road-toreact-project/src/App.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
+function getTitle(title){
+  return title;
+}
+const ToDoList  = [{id:1,title:[],key:id,title:}]
+
 function App(){
   return (
    <div>
-    <h1>Hello World</h1>
-    
+    <h1>ToDoList [{ToDoList}]</h1>
+    <label htmlFor = "search" >Search:</label>
+    <input id = "search"type = "text"/>
    </div>
   );
 }
