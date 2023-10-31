add4675927c63b2d40349cc3acc55a8a9ae942fc
canilo <carlos.will51@gmail.com>
2023-10-30 22:08:01 -0700

    Update App.js
    DAd

--- road-toreact-project/src/App.js
+++ road-toreact-project/src/App.js
@@ -2,14 +2,20 @@
 import * as React from 'react';
 import ToDoList from './TodoList';
 import AddToForm  from './AddTodoForm';
+import { useState } from "react";
+
 function getTitle(title){
   return title;
 }
 function App(){
+  const [newTodo, setNewTodo] = useState('');
+  
+
   return (
    <div>
     <h1>ToDoList</h1>
   <ToDoList />
+<p>{newTodo}</p>
   <AddToForm />
    </div>
 
