ee7bf04c77f985ffef868aae5b13abebaf9abda8
canilo <carlos.will51@gmail.com>
2023-10-30 22:09:03 -0700

    AddToform File
    AddToForm File
    (I got confused with the props(todoTitle)
    Add props as a parameter in the AddTodoForm function
     Inside the handleAddTodo function, invoke the onAddTodo callback prop and pass todoTitle as an argument

--- road-toreact-project/src/AddTodoForm.js
+++ road-toreact-project/src/AddTodoForm.js
@@ -1,9 +1,23 @@
 import React from "react";
-function AddToForm(){
+const handleAddTodo = (event) => {
+event.preventDefault()
+const todoTitle = event.title
+console.log(todoTitle)
+
+}
+
+function AddToForm(props){
+    function handleAddTodo(event){
+        event.preventDefault();
+        const todoTitle = event.title
+        props(todoTitle);
+        console.log(todoTitle)
+        
+    }
     return(
-        <form>
+        <form onsubmit = {handleAddTodo}>
         <label htmlFor="todoTitle">Title</label>
-        <input type="text" id="todoTitle" />
+        <input type="text" name = "title" id="todoTitle" />
         <button type="submit">Add</button>
     </form>
     )
