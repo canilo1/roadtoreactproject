cd4a8e3bdf06398b55e1cb174b86c21930400d3f
canilo <carlos.will51@gmail.com>
2023-11-15 13:44:10 -0800

    Update App.js

--- road-toreact-project/src/App.js
+++ road-toreact-project/src/App.js
@@ -1,44 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-<<<<<<< Updated upstream
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import * as React from 'react';
 import ToDoList from './TodoList';
 import AddToForm  from './AddTodoForm';
 import { useState } from "react";
-function App(){
-  const[todoList,setTodoList] =useState([])
-  function addTodo(newTodo){
-    setTodoList([...todoList,newTodo])
-  }
+import { useEffect } from 'react';
 
+function App(){
+  const[todoList,setTodoList] = useState([])
+  useEffect(() => {
+    localStorage.setItem("todoList",todoList)
+    console.log('todoList has changed:', todoList);
+  }, [todoList]); 
+  const addTodo = (newTodo) => {
+    setTodoList(currTodoList => [...currTodoList,newTodo])
+  }
   return (
    <div>
     <h1>ToDoList</h1>
+    <AddToForm onAddTodo  = {addTodo} />
   <ToDoList todoList = {todoList} />
-  <AddToForm onAddTodo  = {addTodo} />
+
    </div>
 
->>>>>>> Stashed changes
   );
 }
 
